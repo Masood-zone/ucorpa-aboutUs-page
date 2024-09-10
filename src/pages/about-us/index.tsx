135c8db3936a852f843fbef4d6d16d3cffb97626
masood <masoodacheampong@gmail.com>
2024-09-10 21:18:35 +0000

    added a why choose us section

--- src/pages/about-us/index.tsx
+++ src/pages/about-us/index.tsx
@@ -1,9 +1,22 @@
+import Banner from "../../components/banner";
 import WhyChooseUs from "../../components/chooseus";
+import Contributions from "../../components/contributions";
+import Header from "../../components/header";
+import OurTeam from "../../components/our-teams";
 
 function AboutUs() {
   return (
     <section className="min-h-screen bg-white">
+      {/* Header & Breadcrumb */}
+      <Header />
+      {/* Banner */}
+      <Banner />
+      {/* Contributions */}
+      <Contributions />
+      {/* Why Choose us */}
       <WhyChooseUs />
+      {/* Our Team */}
+      <OurTeam />
     </section>
   );
 }
